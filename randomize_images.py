e26bd7b92f5fae715b2fd6de07a15f4a29e96a34
Arisbeth Aguirre <a01274803@itesm.mx>
2024-04-13 15:00:12 -0600

    Added code to randomly rellocate images in folders

---
+++ randomize_images.py
@@ -0,0 +1,22 @@
+import os
+import random
+import shutil
+
+def separar_imagenes_aleatorias(ruta_origen, ruta_destino, numero_imagenes):
+    # Obtener la lista de archivos en la carpeta de origen
+    archivos = os.listdir(ruta_origen)
+
+    # Seleccionar de forma aleatoria el número especificado de imágenes
+    imagenes_seleccionadas = random.sample(archivos, numero_imagenes)
+
+    for imagen in imagenes_seleccionadas:
+        ruta_imagen_origen = os.path.join(ruta_origen, imagen)
+        ruta_imagen_destino = os.path.join(ruta_destino, imagen)
+
+        shutil.move(ruta_imagen_origen, ruta_imagen_destino)
+
+ruta_origen = '/Users/arisbethaguirre/Downloads/PaintingsDataset/NonImpressionism'
+ruta_destino = '/Users/arisbethaguirre/Downloads/PaintingsDataset/train/NonImpressionism'
+numero_imagenes = 1081  # Número de imágenes aleatorias que quieres seleccionar y mover
+
+separar_imagenes_aleatorias(ruta_origen, ruta_destino, numero_imagenes)
